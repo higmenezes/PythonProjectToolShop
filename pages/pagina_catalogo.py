e6c7d46ebe1df8d36d9eecd77df3d80367da4e6d
Higmenezes <higson.menezes@gmail.com>
2026-09-22 21:19:17 -0300

    Version 0.3

--- pages/pagina_catalogo.py
+++ pages/pagina_catalogo.py
@@ -6,8 +6,17 @@ class PaginaCatalogo():
         self.input_busca = page.locator("[data-test=\"search-query\"]")
         self.botao_busca = page.locator("[data-test=\"search-submit\"]")
         self.card_produto = page.locator('[data-test^="product-"]:visible .card-body')
+        self.card_preco = page.locator('[data-test^="product-"]:visible .card-footer').locator("[data-test='product-price']")
         self.select_ordem = page.locator("[data-test=\"sort\"]")
-
 
     def acessar_home(self):
         self.page.goto("")
+        self.page.wait_for_timeout(1000)
+
+    def buscar_produto(self, prod_search):
+        self.input_busca.fill(prod_search)
+        self.botao_busca.click()
+        self.page.wait_for_timeout(1000)
+
+    def filtrar_por_categoria(self, categ_search):
+        self.page.get_by_role("checkbox", name=categ_search).check()
--- tests/test_catalogo.py
+++ tests/test_catalogo.py
@@ -7,7 +7,6 @@ def test_catalogo_busca_produto_existente(page):
     paginacatalogo.input_busca.fill("hammer")
     paginacatalogo.botao_busca.click()
     expect(page.get_by_text("Searched for: hammer")).to_be_visible()
-    page.pause()
     quantidade = paginacatalogo.card_produto.count()
     assert quantidade > 0, f'Quantidade não é maior que zero'
     print(f'Quantidade: {quantidade}')
@@ -16,8 +15,7 @@ def test_catalogo_busca_produto_existente(page):
 def test_catalogo_busca_termo_inexistente(page):
     paginacatalogo = PaginaCatalogo(page)
     paginacatalogo.acessar_home()
-    paginacatalogo.input_busca.fill("hXwrglzd")
-    paginacatalogo.botao_busca.click()
+    paginacatalogo.buscar_produto("hXwrglzd")
     expect(page.get_by_text("Searched for: hXwrglzd")).to_be_visible()
     expect(page.get_by_text("0 products found for 'hXwrglzd'")).to_be_visible()
     expect(page.get_by_text("There are no products found.")).to_be_visible()
@@ -25,14 +23,40 @@ def test_catalogo_busca_termo_inexistente(page):
 def test_catalogo_filtro_categoria(page):
     paginacatalogo = PaginaCatalogo(page)
     paginacatalogo.acessar_home()
-    page.locator("#filters").get_by_text("Hammer").click()
-    '''como validar qualquer aparição da palavra Hammer'''
-    '''fazer validação de categoria completa e não só hammer - buscar como validar várias palavras diferentes'''
-    expect(page.get_by_text("Hammer")).to_be_visible()
+    paginacatalogo.filtrar_por_categoria("Hammer")
+    page.wait_for_timeout(2000)
+    quantidade = paginacatalogo.card_produto.count()
+    print(f'Quantidade: {quantidade}')
+    quantidade_martelos = paginacatalogo.card_produto.get_by_text("Hammer").count()
+    assert quantidade == quantidade_martelos, "Quantidade de cards não é igual a quantidade de cards com martelo"
+    assert quantidade > 0
 
 def test_catalogo_filtro_preco(page):
     paginacatalogo = PaginaCatalogo(page)
     paginacatalogo.acessar_home()
     paginacatalogo.select_ordem.select_option("price,asc")
-    '''Como validar que os preços estão do menor pro maior'''
+    page.wait_for_timeout(2000)
+    prod_preco = paginacatalogo.card_preco.all_text_contents()
+    print(prod_preco)
+    valores = [float(preco.replace('$', '')) for preco in prod_preco]
+    print(valores)
+    for i in range(len(valores) - 1):
+        if valores[i] > valores[i + 1]:
+            assert False, "A lista não está em ordem crescente"
+
+def teste_catalogo_consistencia(page):
+    paginacatalogo = PaginaCatalogo(page)
+    paginacatalogo.acessar_home()
+    page.pause()
+    produto_nome = paginacatalogo.card_produto.locator('[data-test="product-name"]').first.text_content()
+    produto_preco = paginacatalogo.card_preco.first.text_content()
+    paginacatalogo.card_produto.first.click()
+    page.wait_for_timeout(2000)
+    print(produto_nome)
+    print(produto_preco)
+    expect(page.get_by_text(produto_nome).first).to_be_visible()
+    expect(page.get_by_text(produto_preco)).to_be_visible()
+
+
+
     page.pause()
